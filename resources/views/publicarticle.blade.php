cce7351e539c28269c000af329a70906288b19cf
Liesel Downes <lieselta@gmail.com>
2019-04-20 22:27:09 +0930

    test

--- resources/views/publicarticle.blade.php
+++ resources/views/publicarticle.blade.php
@@ -14,7 +14,7 @@
     </style>
     <div class="container article-container">
         <h1>{{$article->title}}</h1>
-        <i><h4>{{\App\User::find($article->user_id)->fname}} {{\App\User::find($article->user_id)->lname}} {{\App\User::find($article->user_id)->id}} | Published {{$article->date}}</h4></i>
+        <h4>{{\App\User::find($article->user_id)->fname}} {{\App\User::find($article->user_id)->lname}} {{\App\User::find($article->user_id)->id}} | Published {{$article->date}}</h4>
         <hr class="my-3">
         <p>{!! html_entity_decode($article->content) !!}</p>
     </div>
