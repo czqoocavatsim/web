70e8ef0ecbbad712d0c348af294b79aee953793a
Laravel Shift <shift@laravelshift.com>
2019-10-06 04:55:09 +0000

    Adopt PHP short array syntax
    Laravel 5 adopted the short array syntax which became available in
    PHP 5.4.

--- app/Http/Controllers/vendor/composer/autoload_psr4.php
+++ app/Http/Controllers/vendor/composer/autoload_psr4.php
@@ -5,11 +5,11 @@
 $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
-return array(
-    'Wohali\\OAuth2\\Client\\' => array($vendorDir . '/wohali/oauth2-discord-new/src'),
-    'Psr\\Http\\Message\\' => array($vendorDir . '/psr/http-message/src'),
-    'League\\OAuth2\\Client\\' => array($vendorDir . '/league/oauth2-client/src'),
-    'GuzzleHttp\\Psr7\\' => array($vendorDir . '/guzzlehttp/psr7/src'),
-    'GuzzleHttp\\Promise\\' => array($vendorDir . '/guzzlehttp/promises/src'),
-    'GuzzleHttp\\' => array($vendorDir . '/guzzlehttp/guzzle/src'),
-);
+return [
+    'Wohali\\OAuth2\\Client\\' => [$vendorDir . '/wohali/oauth2-discord-new/src'],
+    'Psr\\Http\\Message\\' => [$vendorDir . '/psr/http-message/src'],
+    'League\\OAuth2\\Client\\' => [$vendorDir . '/league/oauth2-client/src'],
+    'GuzzleHttp\\Psr7\\' => [$vendorDir . '/guzzlehttp/psr7/src'],
+    'GuzzleHttp\\Promise\\' => [$vendorDir . '/guzzlehttp/promises/src'],
+    'GuzzleHttp\\' => [$vendorDir . '/guzzlehttp/guzzle/src'],
+];
