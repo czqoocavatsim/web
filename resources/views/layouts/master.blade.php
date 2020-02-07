e267e139d227fda82456b09c1e7a9b51f031fb7a
Liesel Downes <liesel.downes@icloud.com>
2020-02-08 09:49:46 +1100

    A fix occurred...

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -322,7 +322,7 @@
         <div class="modal-dialog modal-dialog-centered" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLongTitle"><span class="font-weight-bold red-text"><i class="fas fa-exclamation-circle"></i> An error occured...</span></h5>
+                <h5 class="modal-title" id="exampleModalLongTitle"><span class="font-weight-bold red-text"><i class="fas fa-exclamation-circle"></i> An error occurred...</span></h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
