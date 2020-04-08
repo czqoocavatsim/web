1191ab93dc95a0ce801d86c83ffbb0ce866cbf93
Liesel Downes <liesel.downes@icloud.com>
2020-04-08 20:03:06 +0930

    Fixed resources links

--- resources/views/branding.blade.php
+++ resources/views/branding.blade.php
@@ -4,31 +4,31 @@
 <div class="container py-4">
     <h1 class="font-weight-bold blue-text">Branding</h1>
     <hr>
-    <p>Access our logos and social media icons here. You may use these logos in their published form at your discretion. 
+    <p>Access our logos and social media icons here. You may use these logos in their published form at your discretion.
     We ask however that you do not modify the images from their original, published form. Resizing is permitted, provided the aspect ratio remains unadjusted.
     Please contact our Deputy FIR Chief if you have any queries: <a href="mailto:deputy@czqo.vatcan.ca">deputy@czqo.vatcan.ca.</a></p>
     <h3>Banners</h3>
     <div class="p-2">
         <div class="row">
             <div class="col-md-3 d-flex flex-column align-items-center justify-content-center">
-                <img src="https://resources.czqo.vatcan.ca/pr/brand/banner/ZQO_BANNER_BLUE.png" class="img-fluid" alt="">
+                <img src="https://resources.ganderoceanic.com/pr/brand/banner/ZQO_BANNER_BLUE.png" class="img-fluid" alt="">
                 <br/>
-                <a href="https://resources.czqo.vatcan.ca/pr/brand/banner/ZQO_BANNER_BLUE.png">White on Blue</a>
+                <a href="https://resources.ganderoceanic.com/pr/brand/banner/ZQO_BANNER_BLUE.png">White on Blue</a>
             </div>
             <div class="col-md-3 d-flex flex-column align-items-center justify-content-center">
-                <img src="https://resources.czqo.vatcan.ca/pr/brand/banner/ZQO_BANNER_WHITE.png" class="img-fluid" alt="">
+                <img src="https://resources.ganderoceanic.com/pr/brand/banner/ZQO_BANNER_WHITE.png" class="img-fluid" alt="">
                 <br/>
-                <a href="https://resources.czqo.vatcan.ca/pr/brand/banner/ZQO_BANNER_WHITE.png">Blue on White</a>
+                <a href="https://resources.ganderoceanic.com/pr/brand/banner/ZQO_BANNER_WHITE.png">Blue on White</a>
             </div>
             <div class="col-md-3 d-flex flex-column align-items-center justify-content-center">
-                <img src="https://resources.czqo.vatcan.ca/pr/brand/banner/ZQO_BANNER_TRANSPARENTBLUE.png" class="img-fluid" alt="">
+                <img src="https://resources.ganderoceanic.com/pr/brand/banner/ZQO_BANNER_TRANSPARENTBLUE.png" class="img-fluid" alt="">
                 <br/>
-                <a href="https://resources.czqo.vatcan.ca/pr/brand/banner/ZQO_BANNER_TRANSPARENTBLUE.png">Blue on Transparent</a>
+                <a href="https://resources.ganderoceanic.com/pr/brand/banner/ZQO_BANNER_TRANSPARENTBLUE.png">Blue on Transparent</a>
             </div>
             <div class="col-md-3 d-flex flex-column align-items-center justify-content-center">
-                <img src="https://resources.czqo.vatcan.ca/pr/brand/banner/ZQO_BANNER_TRANSPARENTWHITE.png" class="img-fluid" alt="">
+                <img src="https://resources.ganderoceanic.com/pr/brand/banner/ZQO_BANNER_TRANSPARENTWHITE.png" class="img-fluid" alt="">
                 <br/>
-                <a href="https://resources.czqo.vatcan.ca/pr/brand/banner/ZQO_BANNER_TRANSPARENTWHITE.png">White on Transparent</a>
+                <a href="https://resources.ganderoceanic.com/pr/brand/banner/ZQO_BANNER_TRANSPARENTWHITE.png">White on Transparent</a>
             </div>
         </div>
     </div>
@@ -36,24 +36,24 @@
     <div class="p-2">
         <div class="row">
             <div class="col-md-3 d-flex flex-column align-items-center justify-content-center">
-                <img src="https://resources.czqo.vatcan.ca/pr/brand/square/ZQO_SQUARE_BLUE.png" class="img-fluid" alt="">
+                <img src="https://resources.ganderoceanic.com/pr/brand/square/ZQO_SQUARE_BLUE.png" class="img-fluid" alt="">
                 <br/>
-                <a href="https://resources.czqo.vatcan.ca/pr/brand/square/ZQO_SQUARE_BLUE.png">White on Blue</a>
+                <a href="https://resources.ganderoceanic.com/pr/brand/square/ZQO_SQUARE_BLUE.png">White on Blue</a>
             </div>
             <div class="col-md-3 d-flex flex-column align-items-center justify-content-center">
-                <img src="https://resources.czqo.vatcan.ca/pr/brand/square/ZQO_SQUARE_WHITE.png" class="img-fluid" alt="">
+                <img src="https://resources.ganderoceanic.com/pr/brand/square/ZQO_SQUARE_WHITE.png" class="img-fluid" alt="">
                 <br/>
-                <a href="https://resources.czqo.vatcan.ca/pr/brand/square/ZQO_SQUARE_WHITE.png">Blue on White</a>
+                <a href="https://resources.ganderoceanic.com/pr/brand/square/ZQO_SQUARE_WHITE.png">Blue on White</a>
             </div>
             <div class="col-md-3 d-flex flex-column align-items-center justify-content-center">
-                <img src="https://resources.czqo.vatcan.ca/pr/brand/square/ZQO_SQUARE_TRANSPARENTBLUE.png" class="img-fluid" alt="">
+                <img src="https://resources.ganderoceanic.com/pr/brand/square/ZQO_SQUARE_TRANSPARENTBLUE.png" class="img-fluid" alt="">
                 <br/>
-                <a href="https://resources.czqo.vatcan.ca/pr/brand/square/ZQO_SQUARE_TRANSPARENTBLUE.png">Blue on Transparent</a>
+                <a href="https://resources.ganderoceanic.com/pr/brand/square/ZQO_SQUARE_TRANSPARENTBLUE.png">Blue on Transparent</a>
             </div>
             <div class="col-md-3 d-flex flex-column align-items-center justify-content-center">
-                <img src="https://resources.czqo.vatcan.ca/pr/brand/square/ZQO_SQUARE_TRANSPARENTWHITE.png" class="img-fluid" alt="">
+                <img src="https://resources.ganderoceanic.com/pr/brand/square/ZQO_SQUARE_TRANSPARENTWHITE.png" class="img-fluid" alt="">
                 <br/>
-                <a href="https://resources.czqo.vatcan.ca/pr/brand/squares/ZQO_SQUARE_TRANSPARENTWHITE.png">White on Transparent</a>
+                <a href="https://resources.ganderoceanic.com/pr/brand/squares/ZQO_SQUARE_TRANSPARENTWHITE.png">White on Transparent</a>
             </div>
         </div>
     </div>
@@ -61,24 +61,24 @@
     <div class="p-2">
         <div class="row">
             <div class="col-md-3 d-flex flex-column align-items-center justify-content-center">
-                <img src="https://resources.czqo.vatcan.ca/pr/brand/social/ZQO_SOCIAL_BLUE.png" class="img-fluid" alt="">
+                <img src="https://resources.ganderoceanic.com/pr/brand/social/ZQO_SOCIAL_BLUE.png" class="img-fluid" alt="">
                 <br/>
-                <a href="https://resources.czqo.vatcan.ca/pr/brand/social/ZQO_SOCIAL_BLUE.png">White on Blue</a>
+                <a href="https://resources.ganderoceanic.com/pr/brand/social/ZQO_SOCIAL_BLUE.png">White on Blue</a>
             </div>
             <div class="col-md-3 d-flex flex-column align-items-center justify-content-center">
-                <img src="https://resources.czqo.vatcan.ca/pr/brand/social/ZQO_SOCIAL_WHITE.png" class="img-fluid" alt="">
+                <img src="https://resources.ganderoceanic.com/pr/brand/social/ZQO_SOCIAL_WHITE.png" class="img-fluid" alt="">
                 <br/>
-                <a href="https://resources.czqo.vatcan.ca/pr/brand/social/ZQO_SOCIAL_WHITE.png">Blue on White</a>
+                <a href="https://resources.ganderoceanic.com/pr/brand/social/ZQO_SOCIAL_WHITE.png">Blue on White</a>
             </div>
             <div class="col-md-3 d-flex flex-column align-items-center justify-content-center">
-                <img src="https://resources.czqo.vatcan.ca/pr/brand/social/ZQO_SOCIAL_TRANSPARENTBLUE.png" class="img-fluid" alt="">
+                <img src="https://resources.ganderoceanic.com/pr/brand/social/ZQO_SOCIAL_TRANSPARENTBLUE.png" class="img-fluid" alt="">
                 <br/>
-                <a href="https://resources.czqo.vatcan.ca/pr/brand/social/ZQO_SOCIAL_TRANSPARENTBLUE.png">Blue on Transparent</a>
+                <a href="https://resources.ganderoceanic.com/pr/brand/social/ZQO_SOCIAL_TRANSPARENTBLUE.png">Blue on Transparent</a>
             </div>
             <div class="col-md-3 d-flex flex-column align-items-center justify-content-center">
-                <img src="https://resources.czqo.vatcan.ca/pr/brand/social/ZQO_SOCIAL_TRANSPARENTWHITE.png" class="img-fluid" alt="">
+                <img src="https://resources.ganderoceanic.com/pr/brand/social/ZQO_SOCIAL_TRANSPARENTWHITE.png" class="img-fluid" alt="">
                 <br/>
-                <a href="https://resources.czqo.vatcan.ca/pr/brand/social/ZQO_SOCIAL_TRANSPARENTWHITE.png">White on Transparent</a>
+                <a href="https://resources.ganderoceanic.com/pr/brand/social/ZQO_SOCIAL_TRANSPARENTWHITE.png">White on Transparent</a>
             </div>
         </div>
     </div>
