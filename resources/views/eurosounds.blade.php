b7dc89c15b568368ce86c5ead7a5c161ef24b92f
Liesel Downes <liesel.downes@icloud.com>
2020-04-08 21:46:03 +0930

    option b

--- resources/views/eurosounds.blade.php
+++ resources/views/eurosounds.blade.php
@@ -7,7 +7,6 @@
     <h3 class="font-weight-bold blue-text">An immersive and realistic sound package for the Euroscope ATC Client.</h3>
     <hr>
     <p>Created by Andrew Ogden.</p>
-    <iframe width="560" height="315" src="https://www.youtube.com/embed/lLPmKYXsiP4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br>
     <a href="hhttps://resources.ganderoceanic.com/files/eurosounds/eurosounds-8apr20.zip" role="button" class="btn btn-success mb-4"><i class="fas fa-cloud-download-alt"></i>&nbsp;Download EuroSounds</a>
     <h4 class="font-weight-bold blue-text">Installation Instructions</h4>
     <h5>For individuals:</h5>
@@ -29,5 +28,7 @@
     <p>
 This package is licensed under Creative Commons. Copyright ©️ 2018-2020 - Some Rights Reserved
     </p>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/lLPmKYXsiP4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br>
+
 </div>
 @endsection
