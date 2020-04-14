dffbf443da0579b0ad68a01db15c473b27534741
Liesel Downes <liesel.downes@icloud.com>
2020-04-14 10:13:46 +0930

    Mailgun + new domain for mail

--- config/mail.php
+++ config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'no-reply@czqo.vatcan.ca'),
+        'address' => env('MAIL_FROM_ADDRESS', 'no-reply@ganderoceanic.com'),
         'name' => env('MAIL_FROM_NAME', 'Gander Oceanic VATSIM'),
     ],
 
--- config/services.php
+++ config/services.php
@@ -17,7 +17,6 @@ return [
     'mailgun' => [
         'domain' => env('MAILGUN_DOMAIN'),
         'secret' => env('MAILGUN_SECRET'),
-        'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
     ],
 
     'postmark' => [
