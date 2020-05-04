e799473486f5dbe725e37887cfbe231c31220244
Andrew Ogden <andrewogden@bigpond.com>
2020-05-04 23:20:28 +1000

    because I'm an idiot

--- resources/views/branding.blade.php
+++ resources/views/branding.blade.php
@@ -8,7 +8,7 @@
     We ask however that you do not modify the images from their original, published form. Resizing is permitted, provided the aspect ratio remains unadjusted.
     Please contact our Deputy FIR Chief with any queries: <a href="mailto:deputy@czqo.vatcan.ca">deputy@czqo.vatcan.ca.</a>
     <br/><br/>
-    The hex colour codes used in our branding are <span class="font-weight-bold black-text">#0080C9 (Blue)</span> and <span class="font-weight-bold black-text">#FFFFFF (White)</span>. 
+    The hex colour codes used for our branding are <span class="font-weight-bold black-text">#0080C9 (Blue)</span> and <span class="font-weight-bold black-text">#FFFFFF (White)</span>. 
     The font is <span class="font-weight-bold black-text">Avenir Lt Std</span>.</p>
     <h3>Banners</h3>
     <div class="p-2">
