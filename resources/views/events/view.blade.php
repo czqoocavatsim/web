b1bec87d491403b101825f0be0684df7eb3f757b
Liesel <liesel.downes@icloud.com>
2020-05-12 09:37:11 +0930

    Refreshed public event UI

--- resources/views/events/view.blade.php
+++ resources/views/events/view.blade.php
@@ -11,11 +11,16 @@
 @endif
 
 @section('content')
+    <div class="container-fluid elegant-color-dark py-3">
+        <div class="container">
+            <a href="{{route('events.index')}}" class="white-text" style="font-size: 1.2em;"> <i class="fas fa-arrow-left"></i> All Events</a>
+        </div>
+    </div>
     <div class="card card-image rounded-0 blue" style="@if($event->image_url)background-image: url({{$event->image_url}});@endif background-size: cover; background-position: center;">
-        <div class="text-white text-left py-1 px-4 rgba-black-light">
+        <div class="text-white text-left py-1 px-4 mask rgba-blue-grey-strong">
             <div class="container">
                 <div class="py-5">
-                    <h1 class="h1" style="font-size: 3em;">{{$event->name}}</h1>
+                    <h1 class="font-weight-bold" style="font-size: 3em;">{{$event->name}}</h1>
                     @if ($event->departure_icao && $event->arrival_icao)
                     <h3>{{$event->departure_icao_data()->name}} ({{$event->departure_icao_data()->ICAO}})&nbsp;&nbsp;<i class="fas fa-plane"></i>&nbsp;&nbsp;{{$event->arrival_icao_data()->name}} ({{$event->arrival_icao_data()->ICAO}})</h3>
                     @endif
@@ -26,19 +31,19 @@
     <div class="container py-4">
         <div class="row">
             <div class="col-md-3">
-                <h4>Share</h4>
-                <a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u{{Request::url()}}"><i class="fab blue-text fa-facebook fa-3x"></i></a>
+                <h4 class="font-weight-bold blue-text">Share</h4>
+                <a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u{{Request::url()}}"><i class="fab blue-text fa-facebook fa-2x"></i></a>
                 &nbsp;
-                <a target="_blank" href="https://twitter.com/intent/tweet?text={{$event->name}} - Gander Oceanic FIR VATSIM {{Request::url()}}"><i class="fab blue-text fa-twitter fa-3x"></i></a>
+                <a target="_blank" href="https://twitter.com/intent/tweet?text={{$event->name}} - Gander Oceanic FIR VATSIM {{Request::url()}}"><i class="fab blue-text fa-twitter fa-2x"></i></a>
                 &nbsp;
-                <a target="_blank" href="http://www.reddit.com/submit?url={{Request::url()}}&title={{$event->name}} - Gander Oceanic FIR VATSIM"><i class="fab blue-text fa-reddit fa-3x"></i></a>
+                <a target="_blank" href="http://www.reddit.com/submit?url={{Request::url()}}&title={{$event->name}} - Gander Oceanic FIR VATSIM"><i class="fab blue-text fa-reddit fa-2x"></i></a>
                 &nbsp;
-                <a target="_blank" href="mailto:?subject={{$event->name}}&amp;body={{Request::url()}}"><i class="fas blue-text fa-at fa-3x"></i></a>
-                <h4 class="mt-2">Start Time</h4>
+                <a target="_blank" href="mailto:?subject={{$event->name}}&amp;body={{Request::url()}}"><i class="fas blue-text fa-at fa-2x"></i></a>
+                <h4 class="font-weight-bold blue-text mt-3">Start Time</h4>
                 <p>{{$event->start_timestamp_pretty()}}</p>
-                <h4>End Time</h4>
+                <h4 class="font-weight-bold blue-text mt-3">End Time</h4>
                 <p>{{$event->end_timestamp_pretty()}}</p>
-                <h4>Departure Airport</h4>
+                <h4 class="font-weight-bold blue-text mt-3">Depature Airport</h4>
                 @if (!$event->departure_icao)
                 No departure airport listed.
                 @else
@@ -48,7 +53,7 @@
                     <li>{{$event->departure_icao_data()->regionName}}</li>
                 </ul>
                 @endif
-                <h4>Arrival Airport</h4>
+                <h4 class="font-weight-bold blue-text mt-3">Arrival Airport</h4>
                 @if (!$event->departure_icao)
                 No arrival airport listed.
                 @else
@@ -63,7 +68,7 @@
                 {{$event->html()}}
                 @if (Auth::check() && $event->controller_applications_open && Auth::user()->rosterProfile && !$event->userHasApplied())
                 <br>
-                <h4>Apply to control</h4>
+                <h4 class="font-weight-bold blue-text my-3">Apply to control</h4>
                 <div class="card p-3">
                     <form id="app-form" method="POST" action="{{route('events.controllerapplication.ajax')}}">
                         @csrf
@@ -100,19 +105,17 @@
                 </div>
                 @elseif (Auth::check() && $event->userHasApplied())
                 <br>
-                <h4>Apply to control</h4>
-                <p>You have already applied. Contact the Event Coordinator to change times, or cancel.</p>
+                <h4 class="font-weight-bold blue-text my-3">Apply to control</h4>
+                <p>You have already applied. Contact the Events and Marketing Director to change times, or cancel.</p>
                 @endif
                 <br>
-                <h4>Updates</h4>
+                <h4 class="font-weight-bold blue-text my-3">Updates</h4>
                 @if (count($updates) == 0)
                 None yet!
                 @else
                 @foreach($updates as $u)
                 <div class="card p-3">
-                    <a href="{{Request::url()}}#{{$u->slug}}" name={{$u->slug}}>
-                    <h4>{{$u->title}}</h4>
-                    </a>
+                    <h4 class="">{{$u->title}}</h4>
                     <div class="d-flex flex-row align-items-center">
                         <i class="far fa-clock"></i>&nbsp;&nbsp;Created {{$u->created_pretty()}}</span>&nbsp;&nbsp;•&nbsp;&nbsp;<i class="far fa-user-circle"></i>&nbsp;&nbsp;{{$u->author_pretty()}}
                     </div>
