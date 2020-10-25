e9c562d00fc57090b23e2b131f28b3d62264f092
Liesel D <cocoiadrop@icloud.com>
2020-10-25 23:06:07 +1030

    Revert "Updated about page"
    This reverts commit 915eb53b8d8154f7fe80b5aa31f51ecd13761293.

--- resources/views/about-core.blade.php
+++ resources/views/about-core.blade.php
@@ -1,27 +1,13 @@
 @extends('layouts.master')
 @section('title', 'About CZQO Core - ')
 @section('content')
-<div class="container py-5">
-    <div class="d-flex flex-row justify-content-center">
-        <img src="https://resources.ganderoceanic.com/media/img/brand/sqr/ZQO_SQ_TSPBLUE.png" class="mr-3" style="width: 125px; height: 125px;" alt="">
-        <div class="d-flex flex-column">
-            <h1 class="heading blue-text font-weight-bold display-5">Gander Oceanic Core</h1>
-            <p style="font-size: 1.4em;" class="mb-1">The website for VATSIM's<br>Gander Oceanic OCA</p>
-            <p class="text-muted" style="font-size: 0.9em;">
-                Version {{App\Models\Settings\CoreSettings::where('id', 1)->firstOrFail()->release}} ({{App\Models\Settings\CoreSettings::where('id', 1)->firstOrFail()->sys_build}})
-            </p>
-            <div class="d-flex flex-row align-items-center justify-content-between">
-                <a href="https://github.com/czqoocavatsim" class="text-body d-flex flex-row align-items-center">
-                    <i class="fab fa-2x fa-github mr-2"></i> GitHub
-                </a>
-                <a href="https://github.com/czqoocavatsim/czqo-core/releases" class="text-body d-flex flex-row align-items-center">
-                    <i class="fas fa-history fa-2x mr-2"></i> Change Log
-                </a>
-                <a href="https://canary.ganderoceanic.com" class="text-body d-flex flex-row align-items-center">
-                    <i class="fas fa-2x mr-2 fa-feather-alt"></i> Beta
-                </a>
-            </div>
-        </div>
-    </div>
+<div class="container d-flex py-5 flex-column align-items-center justify-content-center">
+<img src="https://resources.ganderoceanic.com/media/img/brand/sqr/ZQO_SQ_TSPBLUE.png" class="img-fluid" style="width: 125px;" alt="">
+<h1 class="heading blue-text font-weight-bold display-5">Gander Oceanic Core</h1>
+<h4>Release {{App\Models\Settings\CoreSettings::where('id', 1)->firstOrFail()->release}} ({{App\Models\Settings\CoreSettings::where('id', 1)->firstOrFail()->sys_build}})</h4>
+<br>
+<h5 class="text-center"><a href="https://github.com/gander-oceanic-fir-vatsim/czqo-core">View our GitHub</a></h5>
+<h5 class="text-center"><a href="https://blog.ganderoceanic.com/gander-oceanic-core-update-log/">Site Update Log</a></h5>
+<h5 class="text-center"><a href="https://canary.ganderoceanic.com/">Development ("Canary") Website</a></h5>
 </div>
 @endsection
