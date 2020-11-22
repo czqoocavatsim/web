fd9c084494797e3b43078f70317542fe9e57be71
Liesel D <cocoiadrop@icloud.com>
2020-11-22 18:32:47 +1030

    okay

--- resources/views/about/staff.blade.php
+++ resources/views/about/staff.blade.php
@@ -48,7 +48,6 @@
                                 </div>
                             </div>
                         </div>
-                        @endif
                     @endforeach
                 </div>
             @else
