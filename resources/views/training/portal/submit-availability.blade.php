6d709f323d5d1fb6a6a4939b733765d8b3529054
Liesel Downes <cocoiadrop@icloud.com>
2020-12-09 11:48:44 +1100

    Fixed portal dark mode issue

--- resources/views/training/portal/submit-availability.blade.php
+++ resources/views/training/portal/submit-availability.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
 <div class="edge-header blue darken-2" style="background: url(https://camo.githubusercontent.com/d017baa65e14cf81c5cb01ff3e6b263c9e8d0eb04724b1fba4f70e472614e186/68747470733a2f2f692e696d6775722e636f6d2f3435325a654d322e706e67); background-position:center; background-position-y: -450px;"></div>
-<div class="container p-5 white" style="margin-top: -100px; margin-bottom: 5rem !important;">
+<div class="container p-5 {{Auth::user()->preferences->ui_mode == 'light' ? 'light' : ''}}" style="margin-top: -100px; margin-bottom: 5rem !important; {{Auth::user()->preferences->ui_mode == 'dark' ? 'background-color:rgb(47,49,54)' : ''}}">
     <h1 class="blue-text mb-4" style="font-size: 3em;">Submit your availability.</h1>
     <p class="lead">
         Welcome to Gander Oceanic, {{Auth::user()->fullName('F')}}! We're excited to have you with us. Before we start your training, we need you to submit your availability for training sessions for the next couple of weeks. This allows us to assign you an Instructor who is best suited to your timezone.
