f024c4fa0fed6e7430cf69a1c541df1b6481ea4f
Liesel Downes <cocoiadrop@icloud.com>
2020-12-16 14:54:22 +1100

    Updated activity check emails and comparsion code

--- app/Notifications/Network/OneWeekInactivityReminder.php
+++ app/Notifications/Network/OneWeekInactivityReminder.php
@@ -41,9 +41,16 @@ class OneWeekInactivityReminder extends Notification
      */
     public function toMail($notifiable)
     {
-        return (new MailMessage)->view(
-            'emails.network.oneweekinactivityreminder', ['rosterMember' => $this->rosterMember, 'cycle' => $this->cycle]
-        )->subject('[NOTICE] One Week To Fulfil Activity Requirement');
+        return (new MailMessage)
+            ->greeting("Hi {$this->rosterMember->user->fullName('FLC')}")
+            ->line("**One Week Left - Activity Reminder**")
+            ->line("According to our records, you have not yet fulfilled our currency requirement. You require **3 hours** online controlling on Gander or Shanwick by the end of the quarter, otherwise you will be marked inactive.")
+            ->line("There is one week remaining in the current quarter, so this email serves only as a reminder in case you may have forgotten.")
+            ->line("If you have received your certification during this current quarter, then you are exempt from this quarter's requirement.")
+            ->line("Please don’t hesitate to contact us should you have any concerns, or if you need us to make an accommodation. We're here to help!")
+            ->line("*You received this email as there is important information in regard to your status with Gander Oceanic.*")
+            ->salutation("Sent automatically through ActivityBot.")
+            ->subject('[NOTICE] One Week To Fulfil Activity Requirement');
     }
 
     /**
