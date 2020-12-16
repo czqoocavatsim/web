b20031c0ac7ee670d6fe98bff12da15510e8695b
Liesel Downes <cocoiadrop@icloud.com>
2020-12-16 16:35:06 +1100

    Solo certification expiry warnings + UI

---
+++ app/Jobs/ProcessSoloCertExpiryWarnings.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Roster\SoloCertification;
+use App\Models\Settings\CoreSettings;
+use App\Models\Training\Instructing\Instructors\Instructor;
+use App\Notifications\Training\SoloCertifications\SoloCertExpiringStaff;
+use App\Notifications\Training\SoloCertifications\SoloCertExpiringUser;
+use Carbon\Carbon;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Notification;
+use RestCord\DiscordClient;
+
+class ProcessSoloCertExpiryWarnings implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //Get all active solo certs
+        $certs = SoloCertification::where('expires', '>', Carbon::now())->get();
+
+        //Go through certs
+        foreach ($certs as $cert) {
+            //If cert is about to expire
+            if (Carbon::now()->diffInDays($cert->expires) <= 2) {
+                //Discord notification in instructors channel
+                $discord = new DiscordClient(['token' => config('services.discord.token')]);
+                $discord->channel->createMessage([
+                    'channel.id' => intval(config('services.discord.instructors')),
+                    "content" => "",
+                    'embed' => [
+                        "title" => "Solo certification for " . $cert->rosterMember->user->fullName('FLC') . " is about to expire.",
+                        "url" => route('training.admin.solocertifications'),
+                        "timestamp" => Carbon::now(),
+                        "color" => hexdec( "2196f3" ),
+                        "description" => "Expires on " . $cert->expires->toDayDateTimeString() . "."
+                    ]
+                ]);
+
+                //Notify their instructor
+                if ($cert->rosterMember->user->studentProfile && $cert->rosterMember->user->studentProfile->instructor()) {
+                    $cert->rosterMember->user->studentProfile->instructor()->instructor->notify(new SoloCertExpiringStaff($cert));
+                }
+
+                //Notify CI/ACI
+                Notification::route('mail', CoreSettings::find(1)->emailcinstructor)->notify(new SoloCertExpiringStaff($cert));
+                if ($aci = Instructor::where('staff_page_tagline', 'Assistant Chief Instructor')->first()) {
+                    $aci->notify(new SoloCertExpiringStaff($cert));
+                }
+
+                //Notify user
+                $cert->rosterMember->user->notify(new SoloCertExpiringUser($cert));
+            }
+        }
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        //
+    }
+}
