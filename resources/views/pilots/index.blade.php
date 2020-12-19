04a1960a11d6e3ff88fdda68ebed7262a4f7ab00
Liesel Downes <cocoiadrop@icloud.com>
2020-12-19 22:03:13 +1030

    fix

--- resources/views/pilots/index.blade.php
+++ resources/views/pilots/index.blade.php
@@ -1,20 +1,6 @@
-@extends('layouts.primary')
-
-@section('navbarprim')
-
-    @parent
-
-@stop
-@section('title', 'Pilot Tools - ')
-@section('content')
-    <div class="container" style="margin-top: 20px;">
-        <div class="row">
-            <div class="col-md-4">
-                @include('layouts.toolSidebar')
-            </div>
-            <div class="col-md-8">
+@extends('pilots.layout')
+@section('pilot-content')
+<div class="container py-4">
                 <h4>Please select a tool from the left</h4>
             </div>
-        </div>
-    </div>
-@stop
+@endsection
