156fd3e13e0250e996a7e67e0274828bd5c39dc7
Liesel Downes <cocoiadrop@icloud.com>
2021-04-10 18:08:16 +0930

    Update connect.php

--- config/connect.php
+++ config/connect.php
@@ -4,5 +4,5 @@ return [
     'client_id' => env('CONNECT_CLIENT_ID'),
     'secret'    => env('CONNECT_SECRET'),
     'redirect'  => env('CONNECT_REDIRECT_URI'),
-    'endpoint'  => env('CONNECT_ENDPOINT')
+    'endpoint'  => env('CONNECT_ENDPOINT', 'https://auth.vatsim.net')
 ];
