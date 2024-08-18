7cf52712f56e9cfb7db9cf2be47dc8d3ce487a74
JoshuaMicallefYBSU <91457812+JoshuaMicallefYBSU@users.noreply.github.com>
2024-08-19 08:26:21 +1000

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -106,7 +106,7 @@ class Kernel extends ConsoleKernel
         $schedule->job(new ProcessSessionReminders())->daily();
 
         // Check Training Threads Status (Once per week)
-        $schedule->job(new DiscordTrainingUpdates())->weeklyOn(7, '22:25');
+        $schedule->job(new DiscordTrainingUpdates())->weeklyOn(7, '22:28');
 
         // Discord role updating
         //$schedule->job(new UpdateDiscordUserRoles)->twiceDaily(6, 18);
