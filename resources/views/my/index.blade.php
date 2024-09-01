538751d871b66ebae7b70986bd686a93deea6417
JoshuaMicallefYBSU <91457812+JoshuaMicallefYBSU@users.noreply.github.com>
2024-09-01 14:22:05 +1000

    Remove 30min min activity req

--- resources/views/my/index.blade.php
+++ resources/views/my/index.blade.php
@@ -370,7 +370,8 @@
 
                         <h3 class="font-weight-bold blue-text mt-4 pb-2">Your Connections</h3>
                         <p class="mt-2">List of all your Gander Oceanic connections to VATSIM over the last 12 Months.</p>
-                        <p class="mt-0">Connections less than 30 minutes are shown in red, and do not count towards Controller Currency.</p>
+                        {{-- <p class="mt-0">Connections less than 30 minutes are shown in red, and do not count towards Controller Currency.</p> --}}
+                        <p class="mt-0">Connections less than 30 minutes are shown in red.</p>
                         <table class="table dt table-hover table-bordered">
                             <thead>
                                 <th>Position</th>
