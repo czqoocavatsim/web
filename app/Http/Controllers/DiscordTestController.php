d4caf1743c645396cd07d68463708d4c1bb03bca
JoshuaMicallefYBSU <91457812+JoshuaMicallefYBSU@users.noreply.github.com>
2024-10-25 15:34:40 +1000

    DiscordAccountCheck update again

--- app/Http/Controllers/DiscordTestController.php
+++ app/Http/Controllers/DiscordTestController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Services\DiscordClient;
 use Illuminate\Support\Facades\Http;
-use App\Jobs\ProcessShanwickControllers;
+use App\Jobs\DiscordAccountCheck;
 use App\Jobs\ProcessShanwickController;
 
 class DiscordTestController extends Controller
@@ -28,7 +28,7 @@ class DiscordTestController extends Controller
     public function Job()
     {
         // Dispatch the job
-        $job = ProcessShanwickControllers::dispatch();
+        $job = DiscordAccountCheck::dispatch();
 
         // Call the handle method directly to get the result synchronously
         $result = $job->handle();
