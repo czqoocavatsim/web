7332303924171df4c7dffec076ca3e52fae125df
JoshuaMicallefYBSU <joshuamicallef66@gmail.com>
2024-12-01 20:13:12 +1000

    Update ProcessRosterInactivity.php

--- app/Jobs/ProcessRosterInactivity.php
+++ app/Jobs/ProcessRosterInactivity.php
@@ -95,18 +95,18 @@ class ProcessRosterInactivity implements ShouldQueue
             }
 
             // 1NOV - 2 Month Activity Check
-            if($roster->certification == "certified" && $roster->active && Carbon::now()->format('d/m') == "01/12" && $roster->currency < 1) {
+            if($roster->certification == "certified" && $roster->active && Carbon::now()->format('d/m') == "01/11" && $roster->currency < 1) {
                 $active_status = 0;
 
                 $first_names[] = $name;
 
                 $first_notice++;
                 
-                // Notification::send($roster->user, new TwoMonthFromRemoval($roster->user, $currency));
+                Notification::send($roster->user, new TwoMonthFromRemoval($roster->user, $currency));
             }
 
             // 1DEC - 1 Month till Removal
-            if($roster->certification == "certified" && Carbon::now()->format('d/m') == "02/12" && $roster->currency < 1){
+            if($roster->certification == "certified" && Carbon::now()->format('d/m') == "01/12" && $roster->currency < 1){
                 $active_status = 0;
 
                 $second_names[] = $name;
